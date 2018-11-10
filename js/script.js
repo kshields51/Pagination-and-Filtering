a244c88f8f5495ba9ad440f22c50e5f76db221e0
Kevin Shields <kshields551@gmail.com>
2018-11-10 16:53:06 -0500

    this code contains working links

--- js/script.js
+++ js/script.js
@@ -1,50 +1,48 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+const listProfiles = document.getElementsByClassName('student-item');
+const mainDiv = document.querySelector('.page')
+function appendLinks (list) {
+  const allProfiles = list;
+  const numberOfButtons = Math.floor(allProfiles.length/10) + 1;
+  const div = document.createElement('div');
+  div.className = 'pagination';
+  mainDiv.appendChild(div);  //where would this go
+  const ul = document.createElement('ul')
+  div.appendChild(ul)//may need if statement bc how does comp know which div to append to
+  for (var i = 0; i<numberOfButtons; i+=1) {
+    const buttonNumber = i + 1;
 
+    function createLi (buttonNumber) {
+      const linkButtonText = buttonNumber
+      console.log('im running')
+      function createElement(elementName, elementAttribute) {//active will be added later
+        const element = document.createElement(elementName);
+        element[elementAttribute] = "#"
+        element.innerHTML = linkButtonText; 
+        return element;
 
+      }
+      function appendToLi (elementName, elementAttribute) {
+        const element = createElement(elementName, elementAttribute);
+        li.appendChild(element)
+        return element;
+      }
+      const li = document.createElement('li')
+      appendToLi('a', 'href');
+      return(li);
 
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+    }
 
 
 
+    
+    
+    const li = createLi(buttonNumber);
+    ul.appendChild(li);
 
+}
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+  
+}
+appendLinks(listProfiles);
