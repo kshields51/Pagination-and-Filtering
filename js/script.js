f2ef6c14ec6dbc5cafda19594c23c940dbbe1041
Kevin Shields <kshields551@gmail.com>
2018-11-10 20:30:17 -0500

    added the correct buttion func and only displaying 10 at the beginning. Now on to active class status

--- js/script.js
+++ js/script.js
@@ -8,7 +8,8 @@ function appendLinks (list) {
   const div = document.createElement('div');
   div.className = 'pagination';
   mainDiv.appendChild(div);
-  const ul = document.createElement('ul')
+  const ul = document.createElement('ul');
+  ul.id = "pagination-buttons";
   div.appendChild(ul)
   for (var i = 0; i<numberOfButtons; i+=1) {
     const buttonNumber = i + 1;
@@ -40,19 +41,25 @@ appendLinks(listProfiles);
 function showPage (list, page) {
   for (var i = 0; i<=list.length; i+=1) {
     if (i >= (page * 10 - 10) && i < (page * 10)) {
-      list[i].style.display == '';
+      list[i].style.display = '';
 
     } else if (typeof list[i] !== 'undefined') {
       list[i].style.display = 'none';
     } else {
       break;
     }
-      
-    
-
-
   }
-
-
 }
-showPage(listProfiles, 5)
+const ul = document.getElementById('pagination-buttons')
+for (var i = 0; i<=listProfiles.length; i+=1) {
+  if (i >= (0) && i < (10)) {
+    listProfiles[i].style.display = '';
+
+  } else if ( typeof listProfiles[i] !== 'undefined') {
+    listProfiles[i].style.display = 'none';
+  }
+}
+ul.addEventListener('click', (e) => {
+  console.log(e.target);
+  showPage(listProfiles, e.target.textContent);
+});
